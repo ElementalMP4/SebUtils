b087561b6593a944be4814a80e558e3ac1cc7b72
ElementalMP4 <sebahudl@gmail.com>
2026-06-19 10:26:16 +0100

    Paper API headaches

--- src/main/java/elementalmp4/sebutils/listener/PVPListener.java
+++ src/main/java/elementalmp4/sebutils/listener/PVPListener.java
@@ -6,7 +6,9 @@ import main.java.elementalmp4.sebutils.service.GlobalConfigService;
 import main.java.elementalmp4.sebutils.service.PVPToggleService;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -16,23 +18,42 @@ public class PVPListener implements Listener {
 
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent event) {
-        if (!isPlayerVersusPlayer(event)) return;
-        if (GlobalConfigService.getAsBoolean(GlobalConfig.PVP_TOGGLE_ENABLED)) {
-            Player attacker = (Player) event.getDamager();
-            Player target = (Player) event.getEntity();
+        if (!(event.getEntity() instanceof Player target)) return;
 
-            if (PVPToggleService.playerHasDisabledPvp(attacker.getName())) {
-                attacker.sendMessage(Component.text("You cannot attack other players if you have disabled PVP!", NamedTextColor.RED));
-                event.setCancelled(true);
-            } else if (PVPToggleService.playerHasDisabledPvp(target.getName())) {
-                attacker.sendMessage(Component.text(target.getName() + " has PVP disabled!", NamedTextColor.RED));
-                event.setCancelled(true);
-            }
+        Player attacker = resolveAttacker(event.getDamager());
+        if (attacker == null) return;
+
+        if (!GlobalConfigService.getAsBoolean(GlobalConfig.PVP_TOGGLE_ENABLED)) return;
+
+        if (PVPToggleService.playerHasDisabledPvp(attacker.getName())) {
+            attacker.sendMessage(Component.text(
+                    "You cannot attack other players if you have disabled PVP!",
+                    NamedTextColor.RED
+            ));
+            event.setCancelled(true);
+        } else if (PVPToggleService.playerHasDisabledPvp(target.getName())) {
+            attacker.sendMessage(Component.text(
+                    target.getName() + " has PVP disabled!",
+                    NamedTextColor.RED
+            ));
+            event.setCancelled(true);
         }
     }
 
-    private boolean isPlayerVersusPlayer(EntityDamageByEntityEvent event) {
-        return event.getEntity() instanceof Player && event.getDamager() instanceof Player;
-    }
+    private Player resolveAttacker(Entity damager) {
+        // Direct hit from another player
+        if (damager instanceof Player player) {
+            return player;
+        }
 
+        // Projectile (arrow, trident, snowball, etc)
+        if (damager instanceof Projectile projectile) {
+            Object shooter = projectile.getShooter();
+            if (shooter instanceof Player player) {
+                return player;
+            }
+        }
+
+        return null;
+    }
 }
